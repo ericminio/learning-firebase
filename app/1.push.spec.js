8b6adce2f8616fe3a6022d613b7f0ff7fdf9f463
Eric Mignot <eric.mignot@gmail.com>
2017-03-24 12:12:33 -0400

    first green :)

---
+++ app/1.push.spec.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var jsdom = require("jsdom").jsdom;
+var path = require('path').join(__dirname, 'lib/push.js');
+var content = require('fs').readFileSync(path).toString();
+var push = (new Function( content + 'return push;'))();
+
+describe('push', function() {
+
+    var document;
+    beforeEach(function() {
+        document = jsdom('<div id="news"></div>');
+    });
+
+    it('adds the news message', function() {
+        push({id:25, content:{message:'hello world'}}, document);
+        var news = document.querySelector('#news-25.news');
+
+        expect(news.innerHTML).to.equal('hello world');
+    });
+
+    it('pills up the news message', function() {
+        push({id:1, content:{message:'first'}}, document);
+        push({id:2, content:{message:'second'}}, document);
+        var news = document.querySelector('.news');
+
+        expect(news.id).to.equal('news-2');
+    });
+});
